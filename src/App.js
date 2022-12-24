fdd8416d7e9a96bf8ad1282026f848e1f6948564
wishba <wisnubayu34@gmail.com>
2022-12-25 01:27:05 +0700

    base hello world

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [message, setMessage] = useState('text')
+
+  const fetchData = async () => {
+    const result = await axios.get('/.netlify/functions/helloWorld')
+    setMessage(result.data.message)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>{message}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
