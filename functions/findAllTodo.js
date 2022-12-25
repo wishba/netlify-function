d1e107425dad96361b301f8e7453cb1384748b61
wishba <wisnubayu34@gmail.com>
2022-12-25 12:56:21 +0700

    findAllTodo

---
+++ functions/findAllTodo.js
@@ -0,0 +1,29 @@
+const query = require("./utils/query");
+
+const GET_TODOS = `
+  query {
+    allTodos {
+      data {
+        _id
+        title
+        completed
+      }
+    }
+  }  
+`;
+
+exports.handler = async () => {
+  const { data, errors } = await query(GET_TODOS);
+
+  if (errors) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(errors)
+    };
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ graphql: data.allTodos.data })
+  };
+};
