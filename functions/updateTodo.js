6563a5ba6abf96542f070b54cdd9a13fc3f18a0d
wishba <wisnubayu34@gmail.com>
2022-12-25 19:59:25 +0700

    update todo

---
+++ functions/updateTodo.js
@@ -0,0 +1,28 @@
+const query = require("./utils/query")
+
+const UPDATE_TODO = `
+  mutation($id: ID!, $title: String!, $completed: Boolean) {
+    updateTodo(id: $id, data: {title: $title, completed: $completed}) {
+      _id
+      title
+      completed
+    }
+  }
+`
+
+exports.handler = async event => {
+  const { id, title, completed } = JSON.parse(event.body)
+  const { data, errors } = await query(UPDATE_TODO, { id, title, completed })
+
+  if (errors) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(errors)
+    }
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ graphql: data.updateTodo })
+  }
+}
