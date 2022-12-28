8a924dd6f0aa5c454906de90e1879f16948f33b2
wishba <wisnubayu34@gmail.com>
2022-12-28 12:29:59 +0700

    reset

--- src/App.js
+++ src/App.js
@@ -1,50 +1,35 @@
+import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 
 function App() {
 
-  // get all todo
-  const [todos, setTodos] = useState([])
+  const [message, setMessage] = useState('text')
+
+  const fetchData = async () => {
+    const result = await axios.get('/.netlify/functions/helloWorld')
+    setMessage(result.data.message)
+  }
+
   useEffect(() => {
-    async function fetchData() {
-      const response = await axios.get('/api/getTodos')
-      const result = response.data
-      setTodos(result.graphql)
-    }
     fetchData()
   }, [])
 
   return (
     <div className="App">
-      <h1>Todo!</h1>
-
-      {/* form */}
-      <form className='form' action="">
-        <div className="form__flex">
-          <label htmlFor="ftitle">Todo</label>
-          <input className='form__input' type="text" name="ftitle" id="ftitle" />
-        </div>
-        <div className="form__flex">
-          <input type="checkbox" name="fcomplete" id="fcomplete" />
-          <label htmlFor="fcomplete">Complete</label>
-          <input className='form__save form__input' type="submit" value="Save" />
-        </div>
-      </form>
-
-      {/* display all todo */}
-      <ul>
-        {todos.map(todo => (
-          <li key={todo._id}>
-            <p>
-              <input checked={todo.completed ? true : false} type="checkbox" name="" id="" />
-              {todo.title}
-              <button className='list__delete'>X</button>
-            </p>
-          </li>
-        ))}
-      </ul>
-
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>{message}</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
